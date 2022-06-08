53e94de213376d6ba81b51f264859da7459f3880
azzyan <106502700+azzyan@users.noreply.github.com>
2022-06-08 17:10:13 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -28,7 +28,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 iris = pd.read_csv('https://raw.githubusercontent.com/azzyan/finalassignment/main/IRIS.csv')
-X = iris.data.drop('species', axis = 1)
+X = iris.drop('species', axis = 1)
 Y = iris.data['species']
 
 clf = RandomForestClassifier()
