a2114aff7d58ffcc8cc04cbea18ffdbe90cf92f8
azzyan <106502700+azzyan@users.noreply.github.com>
2022-06-09 11:47:22 +0800

    Update futurechallege.py

--- futurechallege.py
+++ futurechallege.py
@@ -27,9 +27,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris = pd.read_csv('https://raw.githubusercontent.com/azzyan/finalassignment/main/IRIS.csv')
+X = iris.drop('species', axis = 1)
+Y = iris['species']
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -38,11 +38,11 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(pd.DataFrame({'species' : ['setosa','versicolor','virginica']})
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+#st.write(iris.target_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
