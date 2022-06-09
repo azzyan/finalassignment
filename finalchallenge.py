51489a1ce887a605b793394e78d0f49b1c3e46b6
azzyan <106502700+azzyan@users.noreply.github.com>
2022-06-09 14:17:20 +0800

    Update finalchallenge.py

--- finalchallenge.py
+++ finalchallenge.py
@@ -3,9 +3,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+#import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
-import plotly.plotly as py
-import plotly.graph_objs as go
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
