be902bf0a975d6f642200d91f447c6b0487bff13
azzyan <106502700+azzyan@users.noreply.github.com>
2022-06-09 11:37:46 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -38,7 +38,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(pd.DataFrame({'species' = ['setosa','versicolor','virginica']})
+st.write(pd.DataFrame({'species' : ['setosa','versicolor','virginica']})
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
