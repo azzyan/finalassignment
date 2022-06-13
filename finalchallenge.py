cac60f2601135d5b640c391116eaafaf8e56243d
azzyan <106502700+azzyan@users.noreply.github.com>
2022-06-14 03:08:08 +0800

    Update finalchallenge.py

--- finalchallenge.py
+++ finalchallenge.py
@@ -35,8 +35,8 @@ st.write(f"## You Have Selected <font color='Aquamarine'>{choice}</font> Dataset
 
 def get_default_dataset(name):
     data = None
-    if name == 'fixed acidity':
-        data = datasets.load_fixed_acidity()
+    if name == 'Iris':
+        data = datasets.load_iris()
     elif name == 'Wine':
         data = datasets.load_wine()
     else:
@@ -53,7 +53,7 @@ def add_dataset_ui(choice_name):
     if choice_name == 'Default':
        dataset_name = st.sidebar.selectbox(
             'Select Dataset',
-            ('fixed acidity', 'Breast Cancer', 'Wine')
+            ('Iris', 'Breast Cancer', 'Wine')
         )
        X, y = get_default_dataset (dataset_name)
        X_names = X
@@ -98,3 +98,4 @@ def add_dataset_ui(choice_name):
     return X,y, X_names, X1
 
 X, y , X_names, cat_var= add_dataset_ui (choice)
+
