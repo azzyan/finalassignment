62ef26c0b7ebe9c0a25e70cbfcb407a7f9f5e52d
azzyan <106502700+azzyan@users.noreply.github.com>
2022-06-14 23:19:20 +0800

    Update finalchallenge.py

--- finalchallenge.py
+++ finalchallenge.py
@@ -35,7 +35,7 @@ st.write(f"## You Have Selected <font color='Aquamarine'>{choice}</font> Dataset
 
 
 def get_default_dataset(name):
-    file = pd.read_csv(https://raw.githubusercontent.com/azzyan/finalassignment/main/winequality-red.csv)
+    file = pd.read_csv('https://raw.githubusercontent.com/azzyan/finalassignment/main/winequality-red.csv')
     if name == 'fixed acidity':
         file = datasets.load_fixed_acidity()
     elif name == 'volatile acidity':
